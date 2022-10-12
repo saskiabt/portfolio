b70c18398545454ea8642ce2b555e8db558f5a9e
saskiabt <sskbinder@gmail.com>
2022-10-12 12:06:22 -0400

    remove router

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles.css";
-// import App from "./App";
+import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./fonts/VictoriannaThint_Web/vtf_victorianna_thin-webfont.ttf";
-import RouteSwitch from "./Components/Routeswitch";
+// import RouteSwitch from "./Components/Routeswitch";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <RouteSwitch />
-    {/* <App /> */}
+    {/* <RouteSwitch /> */}
+    <App />
   </React.StrictMode>,
 );
 
