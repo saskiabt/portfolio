199edd7c1657212a22339f8a5d6c1d3f66991bba
saskiabt <sskbinder@gmail.com>
2022-10-20 12:42:21 -0400

    changr nav button styling and add link to linkedin

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,28 +1,11 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/destructuring-assignment */
 import React from "react";
+import "../styles/nav/nav.css";
 
 function Button(props) {
-  const buttonStyle = {
-    background: "none",
-    border: 0,
-    borderTop: "1px solid black",
-    borderLeft: "1px solid black",
-    borderRight: "1px solid black",
-    fontSize: "24px",
-    fontFamily: "roboto-mono,monospace",
-    fontWeight: 300,
-    fontStyle: "normal",
-    padding: "5px",
-  };
-
   return (
-    <button
-      className="navButton"
-      type="button"
-      onClick={props.onClick}
-      style={buttonStyle}
-    >
+    <button className="navButton" type="button" onClick={props.onClick}>
       {props.text}
     </button>
   );
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,17 @@
 /* eslint-disable react/prop-types */
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import Button from "./Button";
 import headerStyles from "../styles/headerStyles";
 import nameStyles from "../styles/nameStyles";
 import wrapperStyles from "../styles/wrapperStyles";
+import "../styles/nav/nav.css";
 
 function Header({ scrollToAbout, scrollToProjects }) {
+  const goToLinkedin = () => {
+    window.open("https://www.linkedin.com/in/saskia-binder/", "_blank");
+  };
   return (
     <div className="header" style={headerStyles}>
       <div id="name" className="font-face-gm" style={nameStyles}>
@@ -14,6 +20,9 @@ function Header({ scrollToAbout, scrollToProjects }) {
       <div className="button-wrapper" style={wrapperStyles}>
         <Button text="About" onClick={scrollToAbout} />
         <Button text="Projects" onClick={scrollToProjects} />
+        <button type="button" className="navButton" onClick={goToLinkedin}>
+          <FontAwesomeIcon icon={faLinkedin} />
+        </button>
         {/* <Button text="Art" /> */}
       </div>
     </div>
