d33e65b98042aa891a964365a203d2d03e1858e0
saskiabt <sskbinder@gmail.com>
2022-11-07 16:56:25 -0500

    add single card

--- src/Components/Art-page/Artpage.jsx
+++ src/Components/Art-page/Artpage.jsx
@@ -1,9 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Artpage.css";
+import flowers2 from "../../img/artpage/flowers2.jpg";
+import flowers2ZO from "../../img/artpage/flowers2-zoomout.png";
 
 function Artpage() {
+  const [flowers2Source, setFlowers2Source] = useState(flowers2);
+
   return (
-    <div>
-      <h1>Artpage</h1>
+    <div className="ArtPage">
+      <div className="card">
+        <div className="active-image-container">
+          <img
+            className="card-img"
+            src={flowers2Source}
+            alt="colorful painting of flowers by Saskia Binder"
+          />
+        </div>
+        <div className="button-container">
+          <button
+            type="button"
+            className="img-button"
+            onClick={() => setFlowers2Source(flowers2)}
+          >
+            <img
+              src={flowers2}
+              alt="colorful
+                painting
+                of
+                flowers
+                by
+                Saskia
+                Binder"
+            />
+          </button>
+          <button
+            type="button"
+            className="img-button"
+            onClick={() => setFlowers2Source(flowers2ZO)}
+          >
+            <img
+              src={flowers2ZO}
+              alt="colorful
+                painting
+                of
+                flowers
+                by
+                Saskia
+                Binder"
+            />
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
