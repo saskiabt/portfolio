54dbb690a8751480faad41dcb482efc924cc2c73
saskiabt <sskbinder@gmail.com>
2022-11-07 22:56:30 -0500

    add route to portfolio

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const router = createBrowserRouter([
     element: <App />,
     children: [
       {
-        path: "/",
+        path: "/portfolio",
         element: <LandingPage />,
       },
       {
