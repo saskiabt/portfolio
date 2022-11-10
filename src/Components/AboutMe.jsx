847960591cd287fcae7b017451e6d7bb1fb8f063
saskiabt <sskbinder@gmail.com>
2022-11-09 21:36:05 -0500

    update aboutme

--- src/Components/AboutMe.jsx
+++ src/Components/AboutMe.jsx
@@ -28,9 +28,8 @@ const AboutMe = React.forwardRef((props, ref) => {
         in Photoshop, Illustrator, and Figma for UI and design.
       </p>
       <p>
-        This site was built in React with TypeScript & the React Context API.
-        Fonts courtesy of Sébastien Hayez and Jérémy Landes of Velvetyne Type
-        Foundry.
+        This site was built in React with TypeScript & React Context. Fonts
+        courtesy of Sébastien Hayez and Jérémy Landes of Velvetyne Type Foundry.
       </p>
     </div>
   );
